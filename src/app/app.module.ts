03398c5b3e771c50f6cac9722f3a13671705432d
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-08-24 23:33:45 -0400

    cambio de fondo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Index2Component } from './index2/index2.component';
+import { Index1Component } from './index1/index1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Index2Component,
+    Index1Component
   ],
   imports: [
     BrowserModule
