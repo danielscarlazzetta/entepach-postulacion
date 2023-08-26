408d5edec169e2b77ff9ad6164091e940d1674f2
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-08-25 22:44:13 -0400

    finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Index2Component } from './index2/index2.component';
-import { Index1Component } from './index1/index1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Index2Component,
-    Index1Component
   ],
   imports: [
     BrowserModule
--- src/app/index2/index2.component.ts
+++ src/app/index2/index2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer,SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-index2',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class Index2Component {
 
+
+  descargaPDF(BASES_ENTEPACH29: string): void {
+    const nombreArchivo = `${BASES_ENTEPACH29}.pdf`
+    const rutaArchivo = `assets/pdf/${nombreArchivo}`;
+
+    const link = document.createElement('a');
+    link.href = rutaArchivo;
+    link.download = nombreArchivo;
+    link.click();
+  }
+
+  descargaDOCX(FICHA_DE_POSTULACION: string): void {
+    const nombreArchivo = `${FICHA_DE_POSTULACION}.docx`
+    const rutaArchivo = `assets/pdf/${nombreArchivo}`;
+
+    const link = document.createElement('a');
+    link.href = rutaArchivo;
+    link.download = nombreArchivo;
+    link.click();
+  }
+
+  descargaJPG(CONVOCATORIA_ENTEPACH29: string): void {
+    const nombreArchivo = `${CONVOCATORIA_ENTEPACH29}.jpg`
+    const rutaArchivo = `assets/pdf/${nombreArchivo}`;
+
+    const link = document.createElement('a');
+    link.href = rutaArchivo;
+    link.download = nombreArchivo;
+    link.click();
+  }
 }
